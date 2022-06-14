2330db8673ca6827c1a88d0e4934f9bfb03a0f1e
Pavel O <pavelnitel@gmail.com>
2022-06-14 20:02:14 +0300

    Added files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int bilet = 110; // цена билета
+        int priceMile = 20; // количества рублей для одной бонусной милл
+        int bonus = bilet / priceMile;
+        System.out.println((bilet / priceMile) + " бонусных милль");
+
+    }
+}
+
